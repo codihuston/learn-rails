73b59be45291deb5a6ad64a3fa2f885f2be3d928
codi huston <codi.huston540@gmail.com>
2022-02-05 04:13:14 -0600

    Add create/read routes for Articles
    - Includes ActiveRecords for saving into database...

--- blog/src/config/routes.rb
+++ blog/src/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  # This includes all typical (CRUD) routes for a resource
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'welcome/test'
