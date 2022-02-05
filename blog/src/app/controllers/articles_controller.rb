cca9e2e8e303d8174e1ae83952a1be610e546447
codi huston <codi.huston540@gmail.com>
2022-02-05 04:24:44 -0600

    Add article edit / update support

--- blog/src/app/controllers/articles_controller.rb
+++ blog/src/app/controllers/articles_controller.rb
@@ -11,6 +11,21 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # Updates only apply to the given fields
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @article = Article.new(article_params)
    
